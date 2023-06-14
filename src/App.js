f822844ae9a963cc06e733ff8406596f3a4237f1
Shivama-Max <shivamasthakur.02@gmail.com>
2023-06-14 23:42:05 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Table from './Components/DataTable';
+import Table from './Components/Table';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import './App.css'
 
 function App() {
   return (
-    <div class = "container">
+    <div className = "pt-5" id = "bg">
+      <h1 className = "text-center">User Records</h1>
       <Table />
     </div>
   );
